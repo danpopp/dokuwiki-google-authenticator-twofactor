e7b10494992a132b2a449d5635d5b8bf11629bbf
danpopp <dan@danpopp.net>
2014-02-28 10:14:21 -0600

    Create settings.php

---
+++ lang/de/settings.php
@@ -0,0 +1,6 @@
+<?php
+$lang["google_secret"] = 'Google 2 Faktor Authentifizierung Schlüssel';
+$lang["timeout_slack"] = 'Wert für Zeittoleranz (Default: 2 = 2*30sec)';
+$lang["chart_QR_code"] = 'URL zum Google 2 Faktor Authentifizierungs-QR Code (optional)';
+$lang["use2fa_verify"] = 'Aktiviere Google 2 Faktor Authentifizierung';
+?>
